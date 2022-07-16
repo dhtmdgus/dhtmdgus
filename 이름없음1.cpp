5041f5a1b221ae76459ff5636d1be5927b644262
GKM090503 <72422431+NormalHuman-Anything@users.noreply.github.com>
2022-07-16 14:35:03 +0900

    별찍기

--- 이름없음1.cpp
+++ 이름없음1.cpp
@@ -1,9 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
 
-
+	int a;
+	scanf("%d" , &a);
+	
+	for(int i=1;i<=a;i++)
+	{
+		for(int j=i;j<=a;j++) {
+			printf(" ");
+		}
+		for(int k=1;k<=2*i-1;k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
